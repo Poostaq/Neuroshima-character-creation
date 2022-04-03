524a429fa4bdf3b8f3f7eb6dcebbb6d3eed44d2f
Poostaq <piootrek.nowicki@gmail.com>
2022-04-03 23:56:11 +0200

    fixed the bug, selecting value from dropdown selects trait

--- game/EthnicityPage/EthnicityJackOfAllTradesSquaredTrait.gd
+++ game/EthnicityPage/EthnicityJackOfAllTradesSquaredTrait.gd
@@ -1,4 +1,5 @@
 extends "res://EthnicityPage/EthnicityTrait.gd"
+signal ethnicity_secondary_trait_chosen(button)
 
 export var secondary_trait: String
 onready var option_button = $MarginContainer/VBoxContainer/OptionButton
@@ -6,3 +7,4 @@ onready var option_button = $MarginContainer/VBoxContainer/OptionButton
 
 func _on_OptionButton_item_selected(index):
 	secondary_trait = option_button.get_item_text(index)
+	emit_signal("ethnicity_secondary_trait_chosen", self)
