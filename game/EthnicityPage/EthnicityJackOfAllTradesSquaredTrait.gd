d9fa2196b177baf11c5027a7af102e162f5239bb
Poostaq <piootrek.nowicki@gmail.com>
2022-04-08 22:51:21 +0200

    fixed bug + introduced new scrollbar

--- game/EthnicityPage/EthnicityJackOfAllTradesSquaredTrait.gd
+++ game/EthnicityPage/EthnicityJackOfAllTradesSquaredTrait.gd
@@ -1,5 +1,4 @@
 extends "res://EthnicityPage/EthnicityTrait.gd"
-signal ethnicity_secondary_trait_chosen(button)
 
 export var secondary_trait: String
 onready var option_button = $MarginContainer/VBoxContainer/OptionButton
@@ -7,4 +6,5 @@ onready var option_button = $MarginContainer/VBoxContainer/OptionButton
 
 func _on_OptionButton_item_selected(index):
 	secondary_trait = option_button.get_item_text(index)
-	emit_signal("ethnicity_secondary_trait_chosen", self)
+	emit_signal("trait_button_pressed", self)
+	self.set_pressed(true)
