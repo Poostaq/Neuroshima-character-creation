8085434751a09edc25b8dd864d22899fb8121e81
Poostaq <piootrek.nowicki@gmail.com>
2022-04-17 00:19:20 +0200

    Added Dice node for rolling, added rolling value

--- game/AttributesPage/AttributesStep.gd
+++ game/AttributesPage/AttributesStep.gd
@@ -1,16 +1,15 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (NodePath) onready var roll_button = get_node(roll_button) as Button
+export (NodePath) onready var result_label = get_node(result_label) as RichTextLabel
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_RollButton_button_up():
+	var result_rolls = Dices.roll_dice(3, 20)
+	var final_value = ceil((result_rolls[0]+result_rolls[1]+result_rolls[2])/3.0)
+	roll_button.visible = false
+	roll_button.mouse_filter = Control.MOUSE_FILTER_STOP
+	result_label.visible = true
+	result_label.bbcode_text = ("[center] (%s+%s+%s)/3= " % result_rolls) + str(final_value)
