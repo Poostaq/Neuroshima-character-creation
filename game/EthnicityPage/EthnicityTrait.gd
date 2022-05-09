70d3ecc51872f20bdf7fd49ef2c43381381be44d
Poostaq <piootrek.nowicki@gmail.com>
2022-05-10 00:22:32 +0200

    Tooltip Ethnicity

--- game/EthnicityPage/EthnicityTrait.gd
+++ game/EthnicityPage/EthnicityTrait.gd
@@ -4,6 +4,7 @@ signal trait_button_pressed(button)
 export var identifier: String
 export var trait_name: String
 export var description: String
+export var tooltip_text : String
 
 export (NodePath) onready var trait_name_label = get_node(trait_name_label) as RichTextLabel
 export (NodePath) onready var trait_description_label = get_node(trait_description_label) as RichTextLabel
