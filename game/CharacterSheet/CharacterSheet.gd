c773942d4ec5383bf4ff0b02b3036e9bee41dc0b
Poostaq <piootrek.nowicki@gmail.com>
2023-04-01 22:46:00 +0200

    Changed character sheet existing fields

--- game/CharacterSheet/CharacterSheet.gd
+++ game/CharacterSheet/CharacterSheet.gd
@@ -2,54 +2,54 @@ extends Control
 
 
 #AGILITY
-export (NodePath) onready var agility_attribute_value = get_node(agility_attribute_value) as RichTextLabel
-export (NodePath) onready var agi_easy_value = get_node(agi_easy_value) as RichTextLabel
-export (NodePath) onready var agi_normal_value = get_node(agi_normal_value)as RichTextLabel
-export (NodePath) onready var agi_problematic_value = get_node(agi_problematic_value)as RichTextLabel
-export (NodePath) onready var agi_hard_value = get_node(agi_hard_value)as RichTextLabel
-export (NodePath) onready var agi_very_hard_value = get_node(agi_very_hard_value)as RichTextLabel
-export (NodePath) onready var agi_fucking_hard_value = get_node(agi_fucking_hard_value)as RichTextLabel
-export (NodePath) onready var agi_luck_value = get_node(agi_luck_value)as RichTextLabel
+export (NodePath) onready var agility_attribute_value = get_node(agility_attribute_value) as Label
+export (NodePath) onready var agi_easy_value = get_node(agi_easy_value) as Label
+export (NodePath) onready var agi_normal_value = get_node(agi_normal_value)as Label
+export (NodePath) onready var agi_problematic_value = get_node(agi_problematic_value)as Label
+export (NodePath) onready var agi_hard_value = get_node(agi_hard_value)as Label
+export (NodePath) onready var agi_very_hard_value = get_node(agi_very_hard_value)as Label
+export (NodePath) onready var agi_fucking_hard_value = get_node(agi_fucking_hard_value)as Label
+export (NodePath) onready var agi_luck_value = get_node(agi_luck_value)as Label
 
 #PERCEPTION
-export (NodePath) onready var perception_attribute_value = get_node(perception_attribute_value) as RichTextLabel
-export (NodePath) onready var per_easy_value = get_node(per_easy_value) as RichTextLabel
-export (NodePath) onready var per_normal_value = get_node(per_normal_value) as RichTextLabel
-export (NodePath) onready var per_problematic_value = get_node(per_problematic_value) as RichTextLabel
-export (NodePath) onready var per_hard_value = get_node(per_hard_value) as RichTextLabel
-export (NodePath) onready var per_very_hard_value = get_node(per_very_hard_value) as RichTextLabel
-export (NodePath) onready var per_fucking_hard_value = get_node(per_fucking_hard_value) as RichTextLabel
-export (NodePath) onready var per_luck_value = get_node(per_luck_value) as RichTextLabel
+export (NodePath) onready var perception_attribute_value = get_node(perception_attribute_value) as Label
+export (NodePath) onready var per_easy_value = get_node(per_easy_value) as Label
+export (NodePath) onready var per_normal_value = get_node(per_normal_value) as Label
+export (NodePath) onready var per_problematic_value = get_node(per_problematic_value) as Label
+export (NodePath) onready var per_hard_value = get_node(per_hard_value) as Label
+export (NodePath) onready var per_very_hard_value = get_node(per_very_hard_value) as Label
+export (NodePath) onready var per_fucking_hard_value = get_node(per_fucking_hard_value) as Label
+export (NodePath) onready var per_luck_value = get_node(per_luck_value) as Label
 
 #CHARACTER
-export (NodePath) onready var character_attribute_value = get_node(character_attribute_value) as RichTextLabel
-export (NodePath) onready var cha_easy_value = get_node(cha_easy_value) as RichTextLabel
-export (NodePath) onready var cha_normal_value = get_node(cha_normal_value) as RichTextLabel
-export (NodePath) onready var cha_problematic_value = get_node(cha_problematic_value) as RichTextLabel
-export (NodePath) onready var cha_hard_value = get_node(cha_hard_value) as RichTextLabel
-export (NodePath) onready var cha_very_hard_value = get_node(cha_very_hard_value) as RichTextLabel
-export (NodePath) onready var cha_fucking_hard_value = get_node(cha_fucking_hard_value) as RichTextLabel
-export (NodePath) onready var cha_luck_value = get_node(cha_luck_value) as RichTextLabel
+export (NodePath) onready var character_attribute_value = get_node(character_attribute_value) as Label
+export (NodePath) onready var cha_easy_value = get_node(cha_easy_value) as Label
+export (NodePath) onready var cha_normal_value = get_node(cha_normal_value) as Label
+export (NodePath) onready var cha_problematic_value = get_node(cha_problematic_value) as Label
+export (NodePath) onready var cha_hard_value = get_node(cha_hard_value) as Label
+export (NodePath) onready var cha_very_hard_value = get_node(cha_very_hard_value) as Label
+export (NodePath) onready var cha_fucking_hard_value = get_node(cha_fucking_hard_value) as Label
+export (NodePath) onready var cha_luck_value = get_node(cha_luck_value) as Label
 
 #WITS
-export (NodePath) onready var wits_attribute_value = get_node(wits_attribute_value) as RichTextLabel
-export (NodePath) onready var wit_easy_value = get_node(wit_easy_value) as RichTextLabel
-export (NodePath) onready var wit_normal_value = get_node(wit_normal_value) as RichTextLabel
-export (NodePath) onready var wit_problematic_value = get_node(wit_problematic_value) as RichTextLabel
-export (NodePath) onready var wit_hard_value = get_node(wit_hard_value) as RichTextLabel
-export (NodePath) onready var wit_very_hard_value = get_node(wit_very_hard_value) as RichTextLabel
-export (NodePath) onready var wit_fucking_hard_value = get_node(wit_fucking_hard_value) as RichTextLabel
-export (NodePath) onready var wit_luck_value = get_node(wit_luck_value) as RichTextLabel
+export (NodePath) onready var wits_attribute_value = get_node(wits_attribute_value) as Label
+export (NodePath) onready var wit_easy_value = get_node(wit_easy_value) as Label
+export (NodePath) onready var wit_normal_value = get_node(wit_normal_value) as Label
+export (NodePath) onready var wit_problematic_value = get_node(wit_problematic_value) as Label
+export (NodePath) onready var wit_hard_value = get_node(wit_hard_value) as Label
+export (NodePath) onready var wit_very_hard_value = get_node(wit_very_hard_value) as Label
+export (NodePath) onready var wit_fucking_hard_value = get_node(wit_fucking_hard_value) as Label
+export (NodePath) onready var wit_luck_value = get_node(wit_luck_value) as Label
 
 #BODY
-export (NodePath) onready var body_attribute_value = get_node(body_attribute_value) as RichTextLabel
-export (NodePath) onready var bod_easy_value = get_node(bod_easy_value) as RichTextLabel
-export (NodePath) onready var bod_normal_value = get_node(bod_normal_value) as RichTextLabel
-export (NodePath) onready var bod_problematic_value = get_node(bod_problematic_value) as RichTextLabel
-export (NodePath) onready var bod_hard_value = get_node(bod_hard_value) as RichTextLabel
-export (NodePath) onready var bod_very_hard_value = get_node(bod_very_hard_value) as RichTextLabel
-export (NodePath) onready var bod_fucking_hard_value = get_node(bod_fucking_hard_value) as RichTextLabel
-export (NodePath) onready var bod_luck_value = get_node(bod_luck_value) as RichTextLabel
+export (NodePath) onready var body_attribute_value = get_node(body_attribute_value) as Label
+export (NodePath) onready var bod_easy_value = get_node(bod_easy_value) as Label
+export (NodePath) onready var bod_normal_value = get_node(bod_normal_value) as Label
+export (NodePath) onready var bod_problematic_value = get_node(bod_problematic_value) as Label
+export (NodePath) onready var bod_hard_value = get_node(bod_hard_value) as Label
+export (NodePath) onready var bod_very_hard_value = get_node(bod_very_hard_value) as Label
+export (NodePath) onready var bod_fucking_hard_value = get_node(bod_fucking_hard_value) as Label
+export (NodePath) onready var bod_luck_value = get_node(bod_luck_value) as Label
 
 onready var agi_modifiers_elements = [agi_easy_value,agi_normal_value,agi_problematic_value,agi_hard_value,agi_very_hard_value,agi_fucking_hard_value,agi_luck_value]
 onready var per_modifiers_elements = [per_easy_value,per_normal_value,per_problematic_value,per_hard_value,per_very_hard_value,per_fucking_hard_value,per_luck_value]
@@ -60,12 +60,12 @@ onready var bod_modifiers_elements = [bod_easy_value,bod_normal_value,bod_proble
 onready var attribute_modifiers_elements = [agi_modifiers_elements,per_modifiers_elements,cha_modifiers_elements,wit_modifiers_elements,bod_modifiers_elements]
 onready var attribute_value_elements = [agility_attribute_value,perception_attribute_value,character_attribute_value,wits_attribute_value,body_attribute_value]
 
-export (NodePath) onready var name_element = get_node(name_element) as RichTextLabel
-export (NodePath) onready var ethnicity_element = get_node(ethnicity_element) as RichTextLabel
-export (NodePath) onready var ethnicity_trait_element = get_node(ethnicity_trait_element) as RichTextLabel
-export (NodePath) onready var profession_element = get_node(profession_element) as RichTextLabel
-export (NodePath) onready var profession_trait_element = get_node(profession_trait_element) as RichTextLabel
-export (NodePath) onready var specialization_element = get_node(specialization_element) as RichTextLabel
+export (NodePath) onready var name_element = get_node(name_element) as Label
+export (NodePath) onready var ethnicity_element = get_node(ethnicity_element) as Label
+export (NodePath) onready var ethnicity_trait_element = get_node(ethnicity_trait_element) as Label
+export (NodePath) onready var profession_element = get_node(profession_element) as Label
+export (NodePath) onready var profession_trait_element = get_node(profession_trait_element) as Label
+export (NodePath) onready var specialization_element = get_node(specialization_element) as Label
 
 
 onready var db = get_node("/root/DatabaseOperations")
@@ -78,11 +78,11 @@ func update_card():
 
 
 func _update_basic_info_values():
-	ethnicity_element.bbcode_text = CharacterStats.ethnicity
-	ethnicity_trait_element.bbcode_text = CharacterStats.ethnicity_trait
-	profession_element.bbcode_text = CharacterStats.profession
-	profession_trait_element.bbcode_text = CharacterStats.profession_trait
-	specialization_element.bbcode_text = CharacterStats.specialization
+	ethnicity_element.text = CharacterStats.ethnicity
+	ethnicity_trait_element.text = CharacterStats.ethnicity_trait
+	profession_element.text = CharacterStats.profession
+	profession_trait_element.text = CharacterStats.profession_trait
+	specialization_element.text = CharacterStats.specialization
 
 
 func _update_attribute_values(attributeEnum):
@@ -90,20 +90,20 @@ func _update_attribute_values(attributeEnum):
 		return
 	var value = CharacterStats.get_final_attribute_value(CharacterStats.attribute_modifiers_dicts[attributeEnum])
 	CharacterStats.attribute_values_list[attributeEnum] = value
-	attribute_value_elements[attributeEnum].bbcode_text ="[center]%s[/center]" %  value
+	attribute_value_elements[attributeEnum].text ="%s" %  value
 	_fill_attribute_modifiers(value, attribute_modifiers_elements[attributeEnum])
 
 
 func _fill_attribute_modifiers(attribute_value: int, attr_mod_elements: Array):
 	var modifiers = DatabaseOperations.read_list_of_modifiers()
 	for m in range(0, modifiers.size()):
-		attr_mod_elements[m].bbcode_text = _return_modifier_value_or_n(attribute_value + modifiers[m])
+		attr_mod_elements[m].text = _return_modifier_value_or_n(attribute_value + modifiers[m])
 
 
 func _return_modifier_value_or_n(value):
 	if value < 1:
-		return "[center]N[/center]"
-	return str("[center]%s[/center]" %value)
+		return "N"
+	return str("%s" %value)
 
 
 func _on_CloseButton_button_up():
