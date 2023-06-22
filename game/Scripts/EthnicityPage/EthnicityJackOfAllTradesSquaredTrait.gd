9ee44c4d7cc76ba493ebaed551d719f086a00469
Piotr Nowicki <piootrek.nowicki@gmail.com>
2023-06-22 14:17:58 +0200

    fixed one small smelly code

--- game/Scripts/EthnicityPage/EthnicityJackOfAllTradesSquaredTrait.gd
+++ game/Scripts/EthnicityPage/EthnicityJackOfAllTradesSquaredTrait.gd
@@ -4,7 +4,7 @@ export var secondary_trait: String
 export (NodePath) onready var option_button =  get_node(option_button) as OptionButton
 
 
-func _on_OptionButton_item_selected(_index) -> void:
-	secondary_trait = option_button.get_item_text(option_button.selected)
+func _on_OptionButton_item_selected(index) -> void:
+	secondary_trait = option_button.get_item_text(index)
 	emit_signal("trait_button_pressed", self)
 	self.set_pressed(true)
